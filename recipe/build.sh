7b41bcd7d8aa405498a41ad2da99c7ea744777d6
Kristoffer Andersen <kristoffer_andersen@outlook.com>
2025-01-30 14:48:01 +0100

    Update build.sh
    Include dirs plural

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,7 +20,7 @@ cmake ${CMAKE_ARGS} -Wno-dev \
          -D PYTHON_LIBRARY="${PREFIX}/lib/libpython${PY_VER}${SUFFIX}" \
          -D Python_ADDITIONAL_VERSIONS=${CONDA_PY} \
          -D PYTHON_EXECUTABLE:FILEPATH=$PYTHON \
-         -D Python_INCLUDE_DIR=${PREFIX}/include/python${PY_VER} \
+         -D Python_INCLUDE_DIRS=${PREFIX}/include/python${PY_VER} \
          -D COMPILER_CXXFLAGS="-I${PREFIX}/include -w" \
          -D local-castem-header=ON \
          -D enable-fortran=ON \
