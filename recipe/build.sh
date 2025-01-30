baf35cf80e91b9b58bd7a086917e9fc3218de281
Kristoffer Andersen <kristoffer_andersen@outlook.com>
2025-01-30 20:19:48 +0100

    Apply suggestions from code review
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,14 +6,11 @@ echo "**************** M F R O N T  B U I L D  S T A R T S  H E R E ************
 # https://docs.conda.io/projects/conda-build/en/latest/resources/compiler-tools.html#an-aside-on-cmake-and-sysroots
 if [[ "${target_platform}" == osx-* ]]; then
   export LDFLAGS="$LDFLAGS -lm -lpthread -ldl -lz -lomp"
-  export LIBPATH="$PREFIX/lib $LIBPATH"
-  export SUFFIX=.dylib
 else
   export LDFLAGS="$LDFLAGS -L$PREFIX/lib -lm -lpthread -lrt -ldl -lz -lgomp"
-  export LIBPATH="$PREFIX/lib $LIBPATH"
-  export SUFFIX=.so
 fi
 
+export LIBPATH="$PREFIX/lib $LIBPATH"
 cmake ${CMAKE_ARGS} -Wno-dev \
          -DCMAKE_BUILD_TYPE=Release \
          -Dlocal-castem-header=ON \
@@ -23,7 +20,7 @@ cmake ${CMAKE_ARGS} -Wno-dev \
          -DPython_ADDITIONAL_VERSIONS=${CONDA_PY} \
          -DPYTHON_INCLUDE_DIRS=${PREFIX}/include \
          -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_VER} \
-         -DPYTHON_LIBRARY="${PREFIX}/lib/libpython${PY_VER}${SUFFIX}" \
+         -DPYTHON_LIBRARY="${PREFIX}/lib/libpython${PY_VER}${SHLIB_EXT}" \
          -DCOMPILER_CXXFLAGS="-I${PREFIX}/include -w" \
          -Denable-python=ON \
          -Denable-python-bindings=ON \
@@ -32,7 +29,7 @@ cmake ${CMAKE_ARGS} -Wno-dev \
          -DUSE_EXTERNAL_COMPILER_FLAGS=ON \
          -S . -B build
 
-cmake --build ./build --config Release -j 1 # docker gets killed with higher parallelism
+cmake --build ./build --config Release -j ${CPU_COUNT}
 cmake --install ./build --verbose
 
 echo "**************** M F R O N T  B U I L D  E N D S  H E R E ****************"
