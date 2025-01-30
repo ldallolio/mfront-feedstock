5367f5c879ec1a6400ecdebf8ae4e040c064a955
krande <kristoffer_andersen@outlook.com>
2025-01-30 12:12:38 +0100

    add debug printing

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,17 +6,25 @@ echo "**************** M F R O N T  B U I L D  S T A R T S  H E R E ************
 # https://docs.conda.io/projects/conda-build/en/latest/resources/compiler-tools.html#an-aside-on-cmake-and-sysroots
 if [[ "${target_platform}" == osx-* ]]; then
   export LDFLAGS="-L$PREFIX/lib -lm -lpthread -ldl -lz -lomp"
+  export LDFLAGS="$LDFLAGS -Wl,-flat_namespace,-undefined,suppress"
   export LIBPATH="$PREFIX/lib $LIBPATH"
 else
   export LDFLAGS="-L$PREFIX/lib -lm -lpthread -lrt -ldl -lz -lgomp"
   export LIBPATH="$PREFIX/lib $LIBPATH"
 fi
 
+# Debug printing
+echo "PYTHON: $PYTHON"
+echo "PREFIX: $PREFIX"
+echo "CMAKE_ARGS: $CMAKE_ARGS"
+echo "CMAKE_PLATFORM_FLAGS: $CMAKE_PLATFORM_FLAGS"
+
 cmake ${CMAKE_ARGS} -Wno-dev \
          -D CMAKE_PREFIX_PATH=$PREFIX \
          -D CMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
-         -D Python3_EXECUTABLE=$PREFIX/bin/python \
-         -D Python_ADDITIONAL_VERSIONS=${CONDA_PY} \
+         ${CMAKE_PLATFORM_FLAGS[@]} \
+         -D Python3_EXECUTABLE="${PYTHON}" \
+         -D PYTHON_EXECUTABLE:FILEPATH=$PYTHON \
          -D PYTHON_INCLUDE_DIRS="${PREFIX}/include" \
          -D COMPILER_CXXFLAGS="-I${PREFIX}/include -w" \
          -D CMAKE_INSTALL_PREFIX=$PREFIX \
